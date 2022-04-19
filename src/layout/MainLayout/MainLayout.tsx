b87bab33ef045cefd2ecaab4fc2fd8039be9e6ea
lev <lev.domasnix2013@gmail.com>
2022-04-20 01:10:58 +0500

    Сверстал хедер

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,9 +1,11 @@
 import {FC} from "react";
 import {MainLayoutProps} from "./MainLayout.props";
+import {Header} from "../Header/Header";
 
 const MainLayout: FC<MainLayoutProps> = ({children}) => {
     return (
         <div>
+            <Header />
             <main>
                 {children}
             </main>
