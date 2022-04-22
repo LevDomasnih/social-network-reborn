d3f0e5658c9aa1e295cafe61dbfbc98755b7649e
lev <lev.domasnix2013@gmail.com>
2022-04-23 00:16:55 +0500

    Fix

--- src/components/Input/Input.props.tsx
+++ src/components/Input/Input.props.tsx
@@ -2,6 +2,7 @@ import {DetailedHTMLProps, InputHTMLAttributes} from "react";
 import {FieldError} from "react-hook-form";
 
 export interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+    prefix: string
     error?: FieldError
     fontSize?: 'sm' | 'md' | 'xl'
     weight?: 'normal' | 'medium' | 'semibold'
